e3c486b2c8ed8c179f3f74a2cad9a1dda0f313ec
Josh Shih <josh.shih@ARICENT.COM>
2018-08-29 16:19:59 -0700

    finished with maps, starting on interfaces

--- map/main.go
+++ map/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"white": "#ffffff",
+	}
+
+	//delete(colors, "white")
+
+	//fmt.Println(colors)
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println(color, hex)
+	}
+}
