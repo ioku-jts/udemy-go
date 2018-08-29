e1fb577cfaf1bb3e5c1cadba51bb81bd535e2790
Josh Shih <josh.shih@ARICENT.COM>
2018-08-29 13:42:00 -0700

    finished even and odd assignment

--- evenandodd/main.go
+++ evenandodd/main.go
@@ -5,5 +5,12 @@ import (
 )
 
 func main() {
-	fmt.Println("hi there")
+	arr := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	for _, val := range arr {
+		if val%2 == 1 {
+			fmt.Println(val, "is Odd")
+		} else {
+			fmt.Println(val, "is Even")
+		}
+	}
 }
